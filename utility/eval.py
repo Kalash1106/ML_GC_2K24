515f5df8d663e0741f92f35135d18adbeadde95b
Kalash Shah <86906212+Kalash1106@users.noreply.github.com>
2024-03-11 22:26:23 +0530

    Adding the utility files

---
+++ utility/eval.py
@@ -0,0 +1,96 @@
+import torch
+import csv
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from PIL import Image
+import pandas as pd
+import numpy as np
+
+def get_list_from_csv(labels_file):
+    with open(labels_file, newline='') as f:
+        reader = csv.reader(f)
+        labels = list(reader)
+
+    return labels
+
+class ImageLabelDataset(Dataset):
+  def __init__(self, image_folder, labels_file, img_size):
+    self.image_folder = image_folder
+    self.labels = get_list_from_csv(labels_file)
+    self.transform = transforms.Compose([
+                        transforms.Resize(img_size), 
+                        transforms.ToTensor(),
+                        transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+                        ])
+
+  def __len__(self):
+    return len(self.labels)
+
+  def __getitem__(self, idx):
+    # Get image path based on index (assuming filenames correspond to labels)
+    image_path = r"{}\{}.jpg".format(self.image_folder, self.labels[idx][0])  # Modify extension if needed
+    image = Image.open(image_path)
+    image = image.convert('RGB')
+    img_name = self.labels[idx][0]
+
+    # Apply transformations
+    image_tensor = self.transform(image)
+
+    return image_tensor, img_name
+
+class DataUtility:
+  def __init__(self, image_folder, labels_file, img_size = (224, 224)) -> None:
+    self.image_folder = image_folder
+    self.labels_file = labels_file
+    self.img_size = img_size
+    self.dataset = None
+    self.dataloader = None
+
+  def make_dataset(self):
+    self.dataset = ImageLabelDataset(self.image_folder, self.labels_file, self.img_size)
+    return self.dataset
+  
+  def make_dataloader(self, batch_size = 32):
+     #Exception check for dataset creation
+     if self.dataset is None:
+        self.make_dataset()
+
+     self.dataloader = DataLoader(self.dataset, batch_size=batch_size, shuffle=True)
+
+     return self.dataloader
+  
+  def create(self, dataset = True, dataloader = True, batch_size = 32):
+    if dataset:
+      self.make_dataset()
+
+    if dataloader:
+       self.make_dataloader(batch_size)
+
+
+def generate_predictions(model, DataClass, device): 
+    model.eval()
+    model.to(device)
+    
+    predictions = torch.tensor([], device=device)
+    image_ids = []
+
+    with torch.no_grad():
+        for _, metadata in enumerate(DataClass.dataloader):
+            inputs = metadata[0]
+            inputs = inputs.to(device)
+            outputs = model(inputs)
+
+            _, predicted = torch.max(outputs, 1)
+            predictions = torch.cat((predictions, predicted), dim = 0)
+            image_ids += metadata[1]
+        
+    predictions = predictions.cpu()
+    return predictions.numpy().astype(np.int8), image_ids
+    
+
+def make_submission_csv(params, model, device = "cpu", csv_file_path = 'submission.csv', batch_size = 32):
+    DataClassEval = DataUtility(params['eval_image_folder'], params['eval_labels'], params['img_size'])
+    DataClassEval.create(batch_size=batch_size)
+    predictions, image_ids = generate_predictions(model, DataClassEval, device)
+    submission_df = pd.DataFrame({'Image_ID': image_ids, 'Prediction': predictions})
+    submission_df.to_csv(csv_file_path, index=False)
