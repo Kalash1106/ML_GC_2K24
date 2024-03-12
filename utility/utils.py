0a4a1d94754ba78f9c2d2f4e888a7949b849fac7
NiteeshSingh <85400881+NiteeshSingh@users.noreply.github.com>
2024-03-12 16:07:10 +0530

    Create utils.py
    -added function to calculate corresponding class weights

---
+++ utility/utils.py
@@ -0,0 +1,18 @@
+import numpy as np
+from data_gen import simplify_ground_truth
+
+
+def get_weigths(DataClass):
+    labels = np.array(
+        [
+            l["class_id"]
+            for l in simplify_ground_truth(DataClass.gt_file, DataClass.mapping_file)
+        ]
+    )
+    # Find unique elements and their frequencies
+    unique_elements, counts = np.unique(labels, return_counts=True)
+
+    # Calculate inverse frequencies
+    inverse_frequencies = sum(counts) / counts
+    print(unique_elements, inverse_frequencies)
+    return inverse_frequencies
